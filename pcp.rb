8cf3d573092903c77dabac1ceb613d1748465190
JARVIS <parth.verma96@gmail.com>
2018-05-22 01:48:31 +0530

    Fixing errors

--- pcp.rb
+++ pcp.rb
@@ -2,7 +2,7 @@ class Pcp < Formula
   desc "Performance Co-Pilot is a system performance and analysis framework"
   homepage "https://pcp.io/"
   url "https://github.com/Warewolf96/homebrew-pcp/blob/master/pcp-4.1.0.src.tar.gz"
-  sha256 "4d0eab64fb7efd0d3690c51b2dd94f6487f1746423ecd022253a3522ed4a47fd"
+  sha256 "abe9e33af082163673753cf80e9d17931bb068e143f616c11e2ee8e9f5246368"
   depends_on "xz" => :build
   depends_on "cairo"
   depends_on "libmicrohttpd"
