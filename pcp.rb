b31ee1f50e1ac09204fb54a103ffc757db2b20f1
JARVIS <parth.verma96@gmail.com>
2018-05-22 01:42:53 +0530

    Fixing SHAmismatch error

--- pcp.rb
+++ pcp.rb
@@ -2,7 +2,7 @@ class Pcp < Formula
   desc "Performance Co-Pilot is a system performance and analysis framework"
   homepage "https://pcp.io/"
   url "https://github.com/Warewolf96/homebrew-pcp/blob/master/pcp-4.1.0.src.tar.gz"
-  sha256 "e533476aef99661e5ad0ede36992a3e56e109a112fe78d47f702aebaeb8534a4"
+  sha256 "4d0eab64fb7efd0d3690c51b2dd94f6487f1746423ecd022253a3522ed4a47fd"
   depends_on "xz" => :build
   depends_on "cairo"
   depends_on "libmicrohttpd"
