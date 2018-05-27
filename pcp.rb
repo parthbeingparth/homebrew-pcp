ea9aa5d9aab9de192c10a6a3462bbe93b7f848c5
JARVIS <parth.verma96@gmail.com>
2018-05-27 11:47:24 +0530

    Formula Test

--- pcp.rb
+++ pcp.rb
@@ -1,24 +1,22 @@
 class Pcp < Formula
   desc "Performance Co-Pilot is a system performance and analysis framework"
   homepage "https://pcp.io/"
-  url "https://github.com/Warewolf96/homebrew-pcp/blob/master/pcp-4.1.0.src.tar.gz"
-  sha256 "18208af2160fb2086c471d6d2669e517fc8eca4836f91b21ca635de0d2dab3f3"
-  depends_on "xz" => :build
-  depends_on "cairo"
-  depends_on "libmicrohttpd"
-  depends_on "libpng"
-  depends_on "pixman"
+  url "https://github.com/performancecopilot/pcp/archive/4.0.3.tar.gz"
+  sha256 "8464fe0bf9cf8aada61d27b2657e3388bebf619473ccaeed32cb32f636a27d2b"
   depends_on "pkg-config" => :build
+  depends_on "nss" => :recommended
+  depends_on "readline" => :recommended
+  depends_on "qt" => :recommended
+  depends_on "xz" => :recommended
 
   def install
-    system "./configure", "--prefix=/usr/local",
-                          "--sysconfdir=/etc",
-                          "--localstatedir=/var",
-                          "--without-webapi"
+    system "./configure", "--prefix=#{prefix}"
     system "make", "install"
   end
 
   test do
+    # `test do` will create, run in and delete a temporary directory.
+    # Run the test with `brew test pcp`.
     system "false"
   end
 end
