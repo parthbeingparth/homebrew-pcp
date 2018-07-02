4c8fca5e831c69358272e17dac5a1da128e03a7a
JARVIS <parth.verma96@gmail.com>
2018-07-02 16:12:34 +0530

    Final formula for brew

--- pcp.rb
+++ pcp.rb
@@ -1,13 +1,14 @@
 class Pcp < Formula
   desc "Performance Co-Pilot is a system performance and analysis framework"
   homepage "https://pcp.io/"
-  url "https://github.com/performancecopilot/pcp/archive/4.0.3.tar.gz"
-  sha256 "8464fe0bf9cf8aada61d27b2657e3388bebf619473ccaeed32cb32f636a27d2b"
+  url "https://dl.bintray.com/pcp/macosx/pcp-4.0.0-1.dmg"
+  sha256 "8595ea180e7074df0f66dbb586b424966c1967a83f331a90b3e3d7df25f2b20a"
   depends_on "pkg-config" => :build
   depends_on "nss" => :recommended
   depends_on "readline" => :recommended
   depends_on "qt" => :recommended
   depends_on "xz" => :recommended
+  depends_on "python3" => :recommended
 
   def install
     system "./configure", "--prefix=#{prefix}"
