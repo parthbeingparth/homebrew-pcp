435c11920a16ef3ab57ba97521127688c85a0aa1
JARVIS <parth.verma96@gmail.com>
2018-07-06 19:14:44 +0530

    updated formula with brew guidlines

--- pcp.rb
+++ pcp.rb
@@ -1,18 +1,21 @@
 class Pcp < Formula
   desc "Performance Co-Pilot is a system performance and analysis framework"
   homepage "https://pcp.io/"
-  url "https://dl.bintray.com/pcp/macosx/pcp-4.0.0-1.dmg"
-  sha256 "8595ea180e7074df0f66dbb586b424966c1967a83f331a90b3e3d7df25f2b20a"
+  url "https://dl.bintray.com/pcp/source/pcp-4.1.0.src.tar.gz"
+  sha256 "0456e6163dd5a530d6b042bf56e54dc2fa0f9e60c8e7ff559eaa15d362f2d2f2"
   depends_on "pkg-config" => :build
-  depends_on "nss" => :recommended
-  depends_on "readline" => :recommended
-  depends_on "qt" => :recommended
-  depends_on "xz" => :recommended
-  depends_on "python3" => :recommended
+  depends_on "nss"
+  depends_on "python"
+  depends_on "qt"
+  depends_on "readline"
+  depends_on "xz"
 
   def install
     system "./configure", "--prefix=#{prefix}"
     system "make", "install"
+
+    include.install "/usr/local/opt/pcp/lib/"
+    MachO::Tools.change_dylib_id("#{lib}/libpcp_web.dylib.1", "#{lib}/libpcp_webq.1.dylib")
   end
 
   test do
