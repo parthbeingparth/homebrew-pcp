f480a52d58cd1193e6ea0bbb93fac3e47f47bcf6
JARVIS <parth.verma96@gmail.com>
2018-08-06 10:24:38 +0530

    Try run without manager

--- pcp.rb
+++ pcp.rb
@@ -12,7 +12,7 @@ class Pcp < Formula
   depends_on "xz"
 
   def install
-    system "./configure", "--prefix=#{prefix}", , "CC=clang", "CXX=clang"
+    system "./configure", "--prefix=#{prefix}", "CC=clang", "CXX=clang"
   end
 
   plist_options :manual => "pcp"
